1888e2ac57758c6269f8fe32b8fe0df3bc5be264
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2023-02-21 15:34:52 +0800

    clean main and readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Seq 2 Seq Model
 
+I build this model from scratch to learn, and make my hand dirty
+the goal is to build the optimum model so later on I can train it on any kind of
+Questions and Answers my customers will provide me, I can sell the bot 
+as customer support.
+
+**feel free to clone and even open issue if found any error or to improve the model**
+**and don't forget to make start please :)**
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# This is a sample Python script.
 from seq_2_seq2 import Seq2SeqChatbot
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 PATHDATA = "data/AviationQA.csv"
 
 
-def play():
-    # Use a breakpoint in the code line below to debug your script.
+def main():
     chatbot = Seq2SeqChatbot(PATHDATA)
     chatbot.load_data()
     (
@@ -21,8 +17,5 @@ def play():
     print(chatbot.generate_answer("What is your return policy?"))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    play()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
